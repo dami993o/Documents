028992cea632373b0f871ad0e01ebe77d343976f
dami993o <damilola.to@hotmail.com>
2023-03-11 15:41:00 +0000

    first commit

---
+++ test/specs/login.js
@@ -0,0 +1,58 @@
+
+
+describe('My Login application', () => {
+    it('should open Hudl URL', async () => {
+        await browser.url('https://www.hudl.com/home');
+        const expectedText = 'Change the way you see the game.';
+        const element = $('#homepage_hero-button > button');
+        expect(element).toHaveText(expectedText);
+        
+    
+        });
+
+
+    it('Should click login and insert username', async () => {
+        const login =  $('[data-qa-id^="login"]');
+        await login.click();
+        const usernameSelect = $('[data-qa-id^="email-input"]');
+        await  usernameSelect.addValue('damilola.to@hotmail.com');
+
+           
+});
+
+
+it('Should insert password', async () => {
+      const usernameSelect = $('[data-qa-id^="password-input"]');
+      await usernameSelect.addValue('1234Adet');
+});
+
+it('Should click login botton', async () => {
+     const loginbutton =  $('[data-qa-id^="login-btn"]');
+     await loginbutton.click();
+
+
+});
+
+it('verfiy login page', async () => {
+      const expectedText = 'Newcastle Jets FC';
+      const verfiy = $('#ssr-webnav > div > div.hui-webnav__grid.hui-navcontainer > nav.hui-webnav__grid-col--onewhole.hui-primarynav.uni-environment--dark.uni-env--dark > div.hui-primaryteamswitcher > a > div.hui-primaryteamswitcher__display-name');
+      expect(verfiy).toHaveText(expectedText);
+
+    });
+
+
+it('Should click menu botton', async () => {
+    const clicktoprightbtn =  $('.hui-globaluseritem__display-name');
+    await clicktoprightbtn.click();
+
+
+});
+
+it('It should logout', async () => {
+    const logoutbtn =   $('[data-qa-id^="webnav-usermenu-logout"]'); 
+    await logoutbtn.click();
+   
+});
+
+
+});
